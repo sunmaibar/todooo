bb8c9f1a8a53cf004697ebb04ef96ad9ca472837
Jimmy <jajalaba@yahoo.com.tw>
2020-02-23 12:23:50 +0800

    seconddd

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ import TodoInput from "./components/TodoInput";
 
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: "wake up" },
-      { id: 2, title: "keep moving" }
-    ],
+    items: [],
     id: uuid(),
     item: "",
     editItem: false
   };
 
   handleChange = e => {
-    console.log("handle change");
+    this.setState({
+      item: e.target.value
+    });
   };
 
   handleSubmmit = e => {
-    console.log("handle submit");
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    const updatedItems = [...this.state.items, newItem];
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
   };
 
   clearList = () => {
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
-    return <h1>Hello from Todo Input</h1>;
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book"></i>
+              </div>
+            </div>
+            <input
+              value={item}
+              onChange={handleChange}
+              type="text"
+              placeholder="該做什麼呢？"
+              className="form-control text-capitalize"
+            />
+          </div>
+          <button type="submit" className="btn btn-block btn-primary mt-3">
+            新增項目
+          </button>
+        </form>
+      </div>
+    );
   }
 }
